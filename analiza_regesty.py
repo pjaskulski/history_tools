61ce6d7e24d89ee4275fa5658d2198b679ac7703
Piotr Jaskulski <piotr.jaskulski@gmail.com>
2022-05-17 20:28:51 +0200

    inf. o własności

--- analiza_regesty.py
+++ analiza_regesty.py
@@ -21,6 +21,47 @@ slowniki = {1: 'Benedyktyni',
             12: 'Liw'
 }
 
+lista = {
+            r'własn\.\s+szlach\.':'szlachecka',
+            r'własn\.\s+kl\.':'klasztorna',
+            r'własn\.\s+opata':'opata',
+            r'własn\.\s+konw.':'konwentu',
+            r'własn\.\s+ryc\.':'rycerska',
+            r'należy\s+do\s+stołu\s+konw\.':'konwentu',
+            r'należy\s+do\s+bpa\s+lubus\.':'biskupia',
+            r'własn\.\s+podzielona\.':'podzielona',
+            r'wieś\s+szlach\.':'szlachecka',
+            r'należy\s+do\s+kl\.':'klasztorna',
+            r'własn\.\s+wwdy':'wojewody',
+            r'należy\s+do\s+[a-zA-ząśężźćńłó]+\s+stołu\s+opata':'opata',
+            r'należy\s+do\s+stołu\s+opata':'opata',
+            r'własn\.\s+rządu':'rządowa',
+            r'własn\.\s+król\.':'królewska',
+            r'należy\s+do\s+króla':'królewska',
+            r'należy\s+do\s+opactwa':'opata',
+            r'własn\.\s+szlach\.\s+i\s+prep\.':'szlachecka, prepozyta',
+            r'własn\.\s+bpa':'biskupia',
+            r'własn\.\s+szlach\.,\s+następnie\s+kl\.':'szlachecka, klasztorna',
+            r'własn\.\s+bpa[a-zA-Z\.\s]+,\s+następnie\s+rządowa':'biskupia, rządowa',
+            r'własn. kl.[a-zA-Ząśężźźćńłó\.,\s]+pleb\.,\s+[a-zA-Zśężźćńłó]\s+szlach\.':'klasztorna, plebana, szlachecka',
+            r'własn\.\s+kl\.[a-zA-Ząśężźźćńłó\.,\s]+prep\.':'klasztorna, prepozyta',
+            r'własn\.\s+bp':'biskupia',
+            r'własn\.\s+arcbpstwa':'arcybiskupia',
+            r'własn\.\s+książęca':'książęca',
+            r'własn\.\s+książęca[a-zA-Z1-9ąśężźźćńłó\.,\s]+\s+król\.':'książęca, królewska',
+            r'/własn.\s+książęca[a-zA-Ząśężźćńłó,\s]+szlach\./gm':'książęca, szlachecka',
+            r'własn\.\s+książąt':'książęca',
+            r'własn\.\s+książęca[a-zA-Ząśężźćńłó,\s]+kl\.':'książęca, klasztorna'
+        }
+
+
+def remove_html(value: str) -> str:
+    # czyszczenie z pozostałości HTML-a
+    pattern = r'<.*?>'
+    value = re.sub(pattern, '', value)
+    value = value.replace(r'\n', ' ')
+    return value
+
 
 def process_point(sheet, m_id, m_nazwa, point_num, point_text):
     """przetwarzanie treści punktu z zapisem do wskazanego arkusza
@@ -36,15 +77,43 @@ def process_point(sheet, m_id, m_nazwa, point_num, point_text):
     reg_list = []
     point = point_num
 
-    # czyszczenie z pozostałości HTML-a
-    pattern = r'<.*?>'
-    point_text = re.sub(pattern, '', point_text)
-    point_text = point_text.replace(r'\n', ' ')
+    # if 'Własn. szlach. w kluczu melsztyńskim' in point_text:
+    #     print()
 
     reg_list, ownership = get_regesty(point_text)
+
+    reg_list = [remove_html(reg) for reg in reg_list]
+
+    if reg_list:
+        # pierwszy regest może wymagać podzielenia
+        pattern = r'\.\s{1}\d{4}'
+        pattern2 = r'\s+r\.\s{1}\d{4}'
+        match = re.search(pattern, reg_list[0])
+        match2 = re.search(pattern2, reg_list[0])
+        if match and not match2:
+            pos = reg_list[0].find('. 1')
+            if pos != -1:
+                part1 = reg_list[0][:pos + 1]
+                part2 = reg_list[0][pos + 2:]
+                reg_list[0] = part2
+                reg_list.insert(0, part1)
+
     # własność
     if point_num == 3:
-        place_owner[m_nazwa] = ownership
+        for reg in reg_list:
+            year = get_year_from_regest(reg)
+            for pattern, owner_type in lista.items():
+                match = re.search(pattern, reg.lower())
+                if match:
+                    if (m_id, year) in place_owner:
+                        _m_id, _year, owner_types = place_owner[(m_id, year)]
+                        if owner_type not in owner_types:
+                            if not owner_types.endswith(','):
+                                owner_types += ', '
+                            owner_types += owner_type
+                            place_owner[(m_id, year)] = (m_nazwa, year, owner_types)
+                    else:
+                        place_owner[(m_id, year)] = (m_nazwa, year, owner_type)
 
     # specjalna obsługa dla p. 1
     if point_num == 1:
@@ -69,6 +138,7 @@ def process_point(sheet, m_id, m_nazwa, point_num, point_text):
         year = get_year_from_regest(item)
         source = get_source_from_regest(item)
         contents = get_contents_from_regest(item, year, source, m_nazwa)
+        contents = contents.replace('[@', '[').replace('@]',']')
         item = item.replace('[@', '[').replace('@]',']')
         tmp_regest = [m_id, m_nazwa, point, year, contents, source, item]
         regest = tuple(tmp_regest)
@@ -145,9 +215,19 @@ if __name__ == '__main__':
         wb.remove(sheet1)
 
         # arkusz z własnością
-        wlasnosc_sheet = create_sheet(wb=wb, title="Własność", columns=['Miejscowość', 'Własność'])
-        for key, value in place_owner.items():
-            wiersz = (key, value)
+        wlasnosc_sheet = create_sheet(wb=wb, title="Własność", columns=['Miejsc_ID', 'Miejscowość', 'Rok', 'Własność'])
+        for key_tuple, owner_lista in place_owner.items():
+            miejscowosc_id, _year = key_tuple
+            miejscowosc, rok, owner_types = owner_lista
+            
+            # bez powtórzeń typów własności
+            tmp = owner_types.split(',')
+            if len(tmp) > 1:
+                tmp = [t.strip() for t in tmp]
+                tmp = list(set(tmp))
+                owner_types = ', '.join(tmp)
+
+            wiersz = (miejscowosc_id, miejscowosc, rok, owner_types)
             wlasnosc_sheet.append(wiersz)
 
         # zapis pliku xlsx
